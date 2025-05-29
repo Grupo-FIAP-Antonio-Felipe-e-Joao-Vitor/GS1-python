c1793a1a082fe36d58734848f4087975a009fcbb
Antonio <antoniojacintoa@gmail.com>
2025-05-29 16:16:30 -0300

    criando projeto

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+
+def converterCEP(local):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {
+        "q": f"{local}, Brazil",
+        "format": "json",
+        "limit": 1
+    }
+    headers = {
+        "User-Agent": "MeuAppExemplo/1.0 (meuemail@exemplo.com)"
+    }
+
+    resposta = requests.get(url, params=params, headers=headers)
+    dados = resposta.json()
+
+    if dados:
+        lat = dados[0]['lat']
+        lon = dados[0]['lon']
+        return lat, lon
+    else:
+        return None
+
+def mostrarBoasVindas ():
+    print("=" * 65)
+    print("🌧️  Bem-vindo ao Sistema de Previsão de Enchentes do Brasil!  🌊")
+    print("-" * 65)
+
+def mostrarMenu ():
+    print("[1] Procurar risco por CEP")
+    print("[2] Procurar risco por Cidade")
+
+def menu ():
+    escolha = int(input("Deseja especificar a localização como: "))
+    while True:
+        if escolha == 1:
+            localizacao = input("Insira o CEP: ")
+            return localizacao
+        elif escolha == 2:
+            localizacao = input("Insira a cidade com o nome completo (ex: São Paulo, Minas Gerais): ")
+            return localizacao
+        else:
+            print("Digite uma opção válida.")
+            escolha = int(input("Deseja especificar a localização como: "))
+
+mostrarBoasVindas()
+mostrarMenu()
+local = menu()
